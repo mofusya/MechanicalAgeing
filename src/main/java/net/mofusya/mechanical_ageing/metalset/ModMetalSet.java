198244872bbc835f80528f4488e5bf57f3d6ad00
Mofusya <kai10.nakagawa@gmail.com>
2026-03-21 14:44:13 +0900

    added tin, osmium, cobalt, platinum, tungsten, chromium, titanium

--- src/main/java/net/mofusya/mechanical_ageing/metalset/ModMetalSet.java
+++ src/main/java/net/mofusya/mechanical_ageing/metalset/ModMetalSet.java
@@ -14,6 +14,13 @@ public class ModMetalSet {
     public static final MetalSet LEAD = METAL_SET.register("lead", new MetalSet.Builder(11.34, 11, 600, 2022).color(0x7C8BB2));
     public static final MetalSet UNOBTAINIUM = METAL_SET.register("unobtainium", new MetalSet.Builder(5.83, 0.01, Integer.MAX_VALUE, Integer.MAX_VALUE).color(0xDD8FFF));
     public static final MetalSet MITHRIL = METAL_SET.register("mithril", new MetalSet.Builder(10.25, 1570, 2223, 3493).color(0xEED5FF));
+    public static final MetalSet TIN = METAL_SET.register("tin", new MetalSet.Builder(7.31, 13, 505, 2875).color(0xDEDEDE));
+    public static final MetalSet OSMIUM = METAL_SET.register("osmium", new MetalSet.Builder(22.595, 2000, 3306, 5300).color(0x73ADBD));
+    public static final MetalSet COBALT = METAL_SET.register("cobalt", new MetalSet.Builder(8.88, 305, 1768, 3143).color(0x6269D6).magnetic());
+    public static final MetalSet PLATINUM = METAL_SET.register("platinum", new MetalSet.Builder(21.45, 48, 2041, 4098).color(0x869496));
+    public static final MetalSet TUNGSTEN = METAL_SET.register("tungsten", new MetalSet.Builder(10.59, 326.5, 33695, 6203).color(0x7A7A7A));
+    public static final MetalSet CHROMIUM = METAL_SET.register("chromium", new MetalSet.Builder(7.192, Double.MAX_VALUE, 2180, 2945).color(0x333B3D));
+    public static final MetalSet TITANIUM = METAL_SET.register("titanium", new MetalSet.Builder(4.508, 326.5, 1941, 3560).color(0x4F4F4F));
 
     //検索用めも
     /*
